bfa7cf7adc071e42eac9b0018574616d505d7023
Julien Sabathé <https://ytrack.learn.ynov.com/git/sjulien/hangman-web.git>
2023-03-27 08:00:21 +0200

    [ADD] ZOO

--- main.cpp
+++ main.cpp
@@ -1,219 +1,161 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <conio.h>
+#include <cstdlib>
 
+#include "C/Classes/Animal.h"
+#include "C/Classes/Tigre.h"
+#include "C/Classes/Aigle.h"
+#include "C/Classes/Poule.h"
+#include "C/Classes/Coq.h"
+#include "C/Classes/Magasin.h"
 using namespace std;
-
-class Animal
-{
-protected:
-    int temps;
-
-public:
-    Animal()
-    {
-        this->temps = 0;
-    }
-
-    virtual void manger() = 0;
-    virtual void vieillir() = 0;
-
-    bool estVivant()
-    {
-        return true; // tous les animaux sont vivants par défaut
-    }
-
-    int getTemps()
-    {
-        return temps;
-    }
-
-    void incrementerTemps()
-    {
-        temps++;
-    }
-};
-
-class Tigre : public Animal
-{
-protected:
-    string sexe;
-    int age;
-    bool adulte;
-    int temps_de_vie;
-    int temps_restant_avant_mort;
-
-public:
-    Tigre(string sexe)
-    {
-        this->sexe = sexe;
-        this->age = 0;
-        this->adulte = false;
-        this->temps_de_vie = 25;
-        this->temps_restant_avant_mort = this->temps_de_vie;
-    }
-
-    virtual void manger() = 0;
-
-    void vieillir()
-    {
-        this->age += 1;
-        if (this->age >= 6)
-        {
-            this->adulte = true;
-        }
-        this->temps_restant_avant_mort -= 1;
-    }
-
-    bool estVivant()
-    {
-        return (this->temps_restant_avant_mort > 0);
-    }
-
-    bool estAdulte()
-    {
-        return adulte;
-    }
-};
-
-class TigreMale : public Tigre
-{
-private:
-    double alimentation;
-
-public:
-    TigreMale() : Tigre("male")
-    {
-        this->alimentation = 12.0; // en kg de viande par jour
-    }
-
-    void manger()
-    {
-        cout << "Le tigre male mange " << this->alimentation << " kg de viande." << endl;
-    }
-};
-
-class TigreFemelle : public Tigre
-{
-private:
-    double alimentation;
-    bool en_gestation;
-    int temps_gestation;
-    int temps_infertilite;
-    int temps_reproduction;
-
-public:
-    TigreFemelle() : Tigre("femelle")
-    {
-        this->alimentation = 10.0; // en kg de viande par jour
-        this->en_gestation = false;
-        this->temps_gestation = 0;
-        this->temps_infertilite = 0;
-        this->temps_reproduction = 0;
-    }
-
-    void manger()
-    {
-        cout << "Le tigre femelle mange " << this->alimentation << " kg de viande." << endl;
-    }
-
-    bool estEnGestation()
-    {
-        return en_gestation;
-    }
-
-    void demarrerGestation()
-    {
-        this->en_gestation = true;
-        this->temps_gestation = 6; // en mois
-    }
-
-    void terminerGestation()
-    {
-        this->en_gestation = false;
-        this->temps_infertilite = 20; // en mois
-    }
-
-    bool estInfertile()
-    {
-        return (temps_infertilite > 0);
-    }
-
-    bool peutSeReproduire()
-    {
-        return (temps_reproduction == 0 && age >= 6 && age <= 14 && !en_gestation && !estInfertile());
-    }
-
-    void reproduire()
-    {
-        temps_reproduction = 20; // en mois
-    }
-
-    void diminuerTempsInfertilite()
-    {
-        if (temps_infertilite > 0)
-        {
-            temps_infertilite--;
-        }
-    }
-
-    void diminuerTempsReproduction()
-    {
-        if (temps_reproduction > 0)
-        {
-            temps_reproduction--;
-        }
-    }
-};
 
 int main()
 {
-    vector<Tigre*> tigres;
-    TigreMale* male = new TigreMale();
-    TigreFemelle* femelle = new TigreFemelle();
-    tigres.push_back(male);
-    tigres.push_back(femelle);
+    Magasin magasin;
+    cout << "Marchand: " << endl;
+    cout << "Achat d'animaux: "  << endl;
+    cout << "J'ai 80 000 euros" << endl;z
+    auto* Animaux1 = new Animaux("Achat Tigre 6 mois", 3000, nullptr);
+    auto* Animaux2 = new Animaux("Achat Poule 6 mois", 20, nullptr);
+    auto* Animaux3 = new Animaux("Achat Aigle 6 mois", 1000, nullptr);
+    auto* Animaux4 = new Animaux("Achat Coq 6 mois", 100, nullptr);
+    auto* Animaux5 = new Animaux("Achat Tigre 4  ans", 120000.0, nullptr);
+    auto* Animaux6 = new Animaux("Achat Tigre 14 ans", 4000, nullptr);
+    auto* Animaux7 = new Animaux("Achat Aigle 4  ans", 4000, nullptr);
+    auto* Animaux8 = new Animaux("Achat Aigle 14 ans", 2000, nullptr);
+    // Espace
+    auto* Animaux9 = new Animaux("Vente Tigre 6 mois", 1500, nullptr);
+    auto* Animaux10 = new Animaux("Vente Poule 6 mois", 10, nullptr);
+    auto* Animaux11 = new Animaux("Vente Coq 6 mois", 20, nullptr);
+    auto* Animaux12 = new Animaux("Vente Aigle 6 mois", 500, nullptr);
+    auto* Animaux13 = new Animaux("Vente Tigre 4 ans", 60000, nullptr);
+    auto* Animaux15 = new Animaux("Vente Aigle 4 ans", 2000, nullptr);
+    auto* Animaux14 = new Animaux("Vente Tigre 14 ans", 10000, nullptr);
+    auto* Animaux16 = new Animaux("Vente Aigle 14 ans", 400, nullptr);
 
-    int jour = 0; // initialisation du jour
+    magasin.ajouterAnimaux(Animaux1);
+    magasin.ajouterAnimaux(Animaux2);
+    magasin.ajouterAnimaux(Animaux3);
+    magasin.ajouterAnimaux(Animaux4);
+    magasin.ajouterAnimaux(Animaux5);
+    magasin.ajouterAnimaux(Animaux6);
+    magasin.ajouterAnimaux(Animaux7);
+    magasin.ajouterAnimaux(Animaux8);
 
-    while (!tigres.empty())
+    magasin.ajouterAnimaux(Animaux9);
+    magasin.ajouterAnimaux(Animaux10);
+    magasin.ajouterAnimaux(Animaux11);
+    magasin.ajouterAnimaux(Animaux12);
+    magasin.ajouterAnimaux(Animaux13);
+    magasin.ajouterAnimaux(Animaux14);
+    magasin.ajouterAnimaux(Animaux15);
+    magasin.ajouterAnimaux(Animaux16);
+    magasin.afficherAnimaux();
+    getch();
+    vector<Animal *> animaux;
+    animaux.push_back(new TigreMale("male", "cancer"));
+    animaux.push_back(new TigreFemelle("femelle", "diabete"));
+    animaux.push_back(new Aigle("male", "grippe aviaire", nullptr));
+    animaux.push_back(new Aigle("femelle", nullptr, "tuberculose"));
+    animaux.push_back(new Poule("femelle", "coryza"));
+    animaux.push_back(new Coq("femelle", "Maladie De NewCastle"));
+    for (int i = 0; i < 120; i++)
     {
-        ++jour; // incrément du jour à chaque tour de boucle
-
-        cout << "Jour " << jour << ":" << endl;
-
-        for (auto it = tigres.begin(); it != tigres.end(); )
+        // Vieillir tous les animaux
+        for (auto &animal: animaux)
         {
-            Tigre* tigre = *it;
-            tigre->manger();
-            tigre->vieillir();
-            if (!tigre->estVivant())
+            animal->vieillir();
+        }
+
+        int temps_total = 1; // Temps total écoulé dans la simulation
+        while (true)
+        {
+            cout << "Temps total ecoule : " << temps_total << " mois." << endl;
+            for (Animal *animal: animaux)
             {
-                cout << "Le tigre est mort." << endl;
-                delete tigre;
-                it = tigres.erase(it);
+                animal->vieillir();
             }
-            else if (TigreFemelle* femelle = dynamic_cast<TigreFemelle*>(tigre))
+            for (Animal *animal: animaux)
             {
-                if (femelle->peutSeReproduire())
+                animal->mourirDeMaladie();
+            }
+            for (Animal *animal: animaux)
+            {
+                if (auto *femelle = dynamic_cast<TigreFemelle *>(animal))
                 {
-                    cout << "La tigresse est en chaleur et cherche un partenaire." << endl;
-                    for (auto it2 = tigres.begin(); it2 != tigres.end(); ++it2)
+                    femelle->diminuerTempsInfertilite();
+                    femelle->diminuerTempsReproduction();
+                    femelle->estEnGestation();
+                    if (femelle->peutSeReproduire())
                     {
-                        Tigre* partenaire = *it2;
-                        if (partenaire != femelle && dynamic_cast<TigreMale*>(partenaire))
-                        {
-                            cout << "La tigresse a trouvé un partenaire." << endl;
-                            femelle->reproduire();
-                            break;
-                        }
+                        cout << "La femelle tigre se reproduit." << endl;
+                        animaux.push_back(new TigreMale(nullptr, nullptr));
+                        animaux.push_back(new TigreMale(nullptr, nullptr));
+                        cout << "La femelle tigre a donner naissance a 2 tigres males." << endl;
+                        femelle->reproduire();
                     }
                 }
-                femelle->diminuerTempsInfertilite();
-                femelle->diminuerTempsReproduction();
             }
-            ++it;
+            for (auto &animal: animaux)
+            {
+                if (rand() % 100 == 0)   // 1 chance sur 10 de tomber malade
+                {
+                    animal->tomberMalade("Maladie %" + to_string(rand() % 100));
+                }
+            }
+            for (Animal *animal: animaux)
+            {
+                if (auto *male = dynamic_cast<TigreMale *>(animal))
+                {
+                    male->manger();
+                }
+                else if (auto *femelle = dynamic_cast<TigreFemelle *>(animal))
+                {
+                    femelle->diminuerTempsInfertilite();
+                    femelle->diminuerTempsReproduction();
+                }
+                else if (auto *aigle = dynamic_cast<Aigle *>(animal))
+                {
+                    aigle->manger();
+                }
+                else if (auto *poule = dynamic_cast<Poule *>(animal))
+                {
+                    poule->manger();
+                }
+                else if (Coq *coq = dynamic_cast<Coq *>(animal))
+                {
+                    coq->manger();
+                }
+            }
+            animaux.erase(
+                    remove_if(
+                            animaux.begin(),
+                            animaux.end(),
+                            [](Animal *animal)
+                            {
+                                if (!Animal::estVivant())
+                                {
+                                    cout << "Un animal est mort." << endl;
+                                    delete animal;
+                                    return true;
+                                }
+                                return false;
+                            }),
+                    animaux.end()
+            );
+            temps_total++;
+            if (temps_total == 19)
+            {
+                break;
+            }
         }
+        for (Animal *animal: animaux)
+        {
+            delete animal;
+        }
+        return 0;
     }
-
-    return 0;
 }
